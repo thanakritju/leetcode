cd1967b6e1da1e576aa4c8cc5fa8700f900c625e
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2021-08-06 20:21:30 +0700

    Add pieces of code and tests

--- problems/43-trapping-rain-water/trappingrainwater.go
+++ problems/43-trapping-rain-water/trappingrainwater.go
@@ -1,10 +1,32 @@
 package trappingrainwater
 
 func trap(height []int) int {
-	return 0
+	count := 0
+	return count
+}
+
+func isLocalMaxima(index int, height []int) bool {
+	if index == 0 {
+		return height[index] > height[index+1]
+	}
+	if index == len(height)-1 {
+		return height[index-1] < height[index]
+	}
+	return height[index-1] < height[index] && height[index] > height[index+1]
+}
+
+func isLocalMinima(index int, height []int) bool {
+	if index == 0 {
+		return height[index] < height[index+1]
+	}
+	if index == len(height)-1 {
+		return height[index-1] > height[index]
+	}
+	return height[index-1] > height[index] && height[index] < height[index+1]
 }
 
 func getWaterBetweenIndexes(index1 int, index2 int, height []int) int {
+	println(index1, index2)
 	var maxHeight int
 	if height[index1] > height[index2] {
 		maxHeight = height[index2]
@@ -13,7 +35,9 @@ func getWaterBetweenIndexes(index1 int, index2 int, height []int) int {
 	}
 	count := 0
 	for i := index1; i < index2; i++ {
-		count += maxHeight - height[i]
+		if maxHeight >= height[i] {
+			count += maxHeight - height[i]
+		}
 	}
 
 	return count
--- problems/43-trapping-rain-water/trappingrainwater_test.go
+++ problems/43-trapping-rain-water/trappingrainwater_test.go
@@ -12,12 +12,52 @@ func TestTrap(t *testing.T) {
 		expected int
 	}{
 		{[]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, 6},
+		{[]int{0, 1, 0, 2, 2, 2, 1, 0, 1, 3, 2, 1, 2, 1}, 6},
 		{[]int{4, 2, 0, 3, 2, 5}, 9},
 	}
 
 	for _, test := range tests {
 		actual := trap(test.input)
-		assert.Equal(t, test.expected, actual)
+		assert.Equal(t, test.expected, actual, test)
+	}
+}
+
+func TestIsLocalMaxima(t *testing.T) {
+	var tests = []struct {
+		inputIndex int
+		inputArray []int
+		expected   bool
+	}{
+		{0, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, false},
+		{1, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, true},
+		{3, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, true},
+		{7, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, true},
+		{1, []int{0, 1}, true},
+	}
+
+	for _, test := range tests {
+		actual := isLocalMaxima(test.inputIndex, test.inputArray)
+		assert.Equal(t, test.expected, actual, test)
+	}
+}
+
+func TestIsLocalMinima(t *testing.T) {
+	var tests = []struct {
+		inputIndex int
+		inputArray []int
+		expected   bool
+	}{
+		{2, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, true},
+		{5, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, true},
+		{7, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, false},
+		{1, []int{2, 1}, true},
+		{0, []int{2, 1}, false},
+		{3, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, false},
+	}
+
+	for _, test := range tests {
+		actual := isLocalMinima(test.inputIndex, test.inputArray)
+		assert.Equal(t, test.expected, actual, test)
 	}
 }
 
@@ -31,10 +71,11 @@ func TestGetWaterBetweenIndexes(t *testing.T) {
 		{3, 7, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, 4},
 		{1, 3, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, 1},
 		{8, 10, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, 1},
+		{7, 10, []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, 1},
 	}
 
 	for _, test := range tests {
 		actual := getWaterBetweenIndexes(test.inputIndex1, test.inputIndex2, test.inputArray)
-		assert.Equal(t, test.expected, actual)
+		assert.Equal(t, test.expected, actual, test)
 	}
 }
