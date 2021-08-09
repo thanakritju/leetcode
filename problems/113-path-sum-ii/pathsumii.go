fad45462cf1ce7454620bd65dab050c73ee39134
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2021-08-10 00:52:30 +0700

    Finished path sum ii

--- problems/113-path-sum-ii/pathsumii.go
+++ problems/113-path-sum-ii/pathsumii.go
@@ -7,5 +7,28 @@ type TreeNode struct {
 }
 
 func pathSum(root *TreeNode, targetSum int) [][]int {
-	return [][]int{}
+	return getAllArrays(root, [][]int{}, []int{}, targetSum)
+}
+
+func getAllArrays(root *TreeNode, arrays [][]int, array []int, targetSum int) [][]int {
+	if root != nil {
+		array = append(array, root.Val)
+		if root.Left == nil && root.Right == nil && sumArray(array) == targetSum {
+			copiedArray := make([]int, len(array))
+			copy(copiedArray, array)
+			arrays = append(arrays, copiedArray)
+		} else {
+			arrays = getAllArrays(root.Left, arrays, array, targetSum)
+			arrays = getAllArrays(root.Right, arrays, array, targetSum)
+		}
+	}
+	return arrays
+}
+
+func sumArray(array []int) int {
+	sum := 0
+	for _, each := range array {
+		sum += each
+	}
+	return sum
 }
--- problems/113-path-sum-ii/pathsumii_test.go
+++ problems/113-path-sum-ii/pathsumii_test.go
@@ -1,6 +1,7 @@
 package pathsumii
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,16 +9,32 @@ import (
 
 func TestPathSum(t *testing.T) {
 	var tests = []struct {
-		inputArray     []int
+		inputArray     []string
 		inputTargetSum int
 		expected       [][]int
 	}{
-		{[]int{1, 2}, 0, [][]int{}},
-		{[]int{1, 2, 3}, 5, [][]int{}},
+		{[]string{"5", "4", "8", "11", "null", "13", "4", "7", "2", "null", "null", "null", "null", "5", "1"}, 22, [][]int{{5, 4, 11, 2}, {5, 8, 4, 5}}},
+		{[]string{"1", "2"}, 0, [][]int{}},
+		{[]string{"1", "2", "3"}, 5, [][]int{}},
 	}
 
 	for _, test := range tests {
-		actual := pathSum(&TreeNode{}, test.inputTargetSum)
+		root := insertLevelOrder(test.inputArray, &TreeNode{}, 0, len(test.inputArray))
+		actual := pathSum(root, test.inputTargetSum)
 		assert.Equal(t, test.expected, actual, test)
 	}
+}
+
+func insertLevelOrder(arr []string, root *TreeNode, i int, n int) *TreeNode {
+	if i < n {
+		value, err := strconv.Atoi(arr[i])
+		if err != nil {
+			return root
+		}
+		temp := TreeNode{Val: value}
+		root = &temp
+		root.Left = insertLevelOrder(arr, root.Left, 2*i+1, n)
+		root.Right = insertLevelOrder(arr, root.Right, 2*i+2, n)
+	}
+	return root
 }
