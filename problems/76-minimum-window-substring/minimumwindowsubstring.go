f4af310a60045b11f04d979ea1c98a174c2470bb
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2021-08-16 00:10:29 +0700

    add challenge of the week

---
+++ problems/76-minimum-window-substring/minimumwindowsubstring.go
@@ -0,0 +1,11 @@
+package minimumwindowsubstring
+
+func minWindow(s string, t string) string {
+	lenS := len(s)
+	lenT := len(t)
+	if lenT > lenS {
+		return ""
+	}
+
+	return ""
+}
---
+++ problems/76-minimum-window-substring/minimumwindowsubstring_test.go
@@ -0,0 +1,25 @@
+package minimumwindowsubstring
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindOrder(t *testing.T) {
+	var tests = []struct {
+		inputS   string
+		inputT   string
+		expected string
+	}{
+		{"ADOBECODEBANC", "ABC", "BANC"},
+		{"a", "a", "a"},
+		{"a", "aa", ""},
+		{"ab", "ba", "ab"},
+	}
+
+	for _, test := range tests {
+		actual := minWindow(test.inputS, test.inputT)
+		assert.Equal(t, test.expected, actual, test)
+	}
+}
