bfe3177eb360ab1cf4274c3a0833c3814c69caa0
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2021-09-11 20:12:36 +0700

    init prob

---
+++ problems/1584-min-cost-to-connect-all-points/mincostconnectpoints.go
@@ -0,0 +1,22 @@
+package mincosttoconnectallpoints
+
+import "math"
+
+func minCostConnectPoints(points [][]int) int {
+	g := graph{}
+	return 0
+}
+
+func distance(x1 int, y1 int, x2 int, y2 int) int {
+	return int(math.Abs(float64(x1-x2)) + math.Abs(float64(y1-y2)))
+}
+
+type graph struct {
+	edges map[int][]edge
+}
+
+type edge struct {
+	nodeID int
+	weight int
+	next   int
+}
---
+++ problems/1584-min-cost-to-connect-all-points/mincostconnectpoints_test.go
@@ -0,0 +1,25 @@
+package mincosttoconnectallpoints
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMinCostConnectPoints(t *testing.T) {
+	var tests = []struct {
+		input    [][]int
+		expected int
+	}{
+		{[][]int{{0, 0}}, 0},
+		{[][]int{{3, 12}, {-2, 5}, {-4, 1}}, 18},
+		{[][]int{{0, 0}, {2, 2}, {3, 10}, {5, 2}}, 20},
+		{[][]int{{0, 0}, {1, 1}, {1, 0}, {-1, 1}}, 4},
+		{[][]int{{-1000000, -1000000}, {1000000, 1000000}}, 4000000},
+	}
+
+	for _, test := range tests {
+		actual := minCostConnectPoints(test.input)
+		assert.Equal(t, test.expected, actual, test)
+	}
+}
