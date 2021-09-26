929a1a78acb2c2f6a427b562fda4f45026e17a36
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2021-09-26 23:24:44 +0700

    course schedule ii topology sorting using adj matrix

--- problems/210-course-schedule-ii/coursescheduleii.go
+++ problems/210-course-schedule-ii/coursescheduleii.go
@@ -1,5 +1,58 @@
 package coursescheduleii
 
+var adjMatrix [2000][2000]int
+var sortedList []int
+var foundCyclic bool
+
+const (
+	NOT_VISITED = iota
+	TEMPORARY
+	PERMANENT
+)
+
 func findOrder(numCourses int, prerequisites [][]int) []int {
-	return []int{}
+	adjMatrix = [2000][2000]int{}
+	for _, v := range prerequisites {
+		adjMatrix[v[1]][v[0]] = 1
+	}
+	visited := make(map[int]int)
+	sortedList = []int{}
+	foundCyclic = false
+
+	for i := 0; i < numCourses; i++ {
+		_, v := visited[i]
+		if v {
+			continue
+		}
+		if !foundCyclic {
+			visit(i, numCourses, visited)
+		}
+	}
+
+	if foundCyclic {
+		return []int{}
+	} else {
+		return sortedList
+	}
+}
+
+func visit(n int, numCourses int, visited map[int]int) {
+	v := visited[n]
+	if v == PERMANENT {
+		return
+	}
+	if v == TEMPORARY {
+		foundCyclic = true
+		return
+	}
+	visited[n] = TEMPORARY
+
+	for i := 0; i < numCourses; i++ {
+		if adjMatrix[n][i] == 1 {
+			visit(i, numCourses, visited)
+		}
+	}
+
+	visited[n] = PERMANENT
+	sortedList = append([]int{n}, sortedList...)
 }
--- problems/210-course-schedule-ii/coursescheduleii_test.go
+++ problems/210-course-schedule-ii/coursescheduleii_test.go
@@ -12,8 +12,10 @@ func TestFindOrder(t *testing.T) {
 		inputPrerequisites [][]int
 		expected           []int
 	}{
-		{5, [][]int{{1, 4}, {2, 4}, {3, 1}, {3, 2}}, []int{0, 1, 2, 3}},
+		{4, [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}}, []int{0, 2, 1, 3}},
 		{2, [][]int{{1, 0}}, []int{0, 1}},
+		{2, [][]int{{1, 0}, {0, 1}}, []int{}},
+		{1, [][]int{}, []int{0}},
 	}
 
 	for _, test := range tests {
