023817724538f505c76c460441c14fecbf7212a4
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2022-03-03 10:58:14 +0700

    save

--- problems/863-all-nodes-distance-k-in-binary-tree/distancek.go
+++ problems/863-all-nodes-distance-k-in-binary-tree/distancek.go
@@ -7,5 +7,21 @@ type TreeNode struct {
 }
 
 func distanceK(root *TreeNode, target *TreeNode, k int) []int {
+	target := searchBST(root, target.Val)
 	return []int{}
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root != nil {
+		if root.Val == val {
+			return root
+		}
+		if temp := searchBST(root.Left, val); temp != nil {
+			return temp
+		}
+		if temp := searchBST(root.Right, val); temp != nil {
+			return temp
+		}
+	}
+	return nil
 }
