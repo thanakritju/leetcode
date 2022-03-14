b33630fe7b4d7e53fdf868caf6bc3d035ed0bd3c
thanakritju <thanakrit.ju.work@gmail.com>
2022-03-14 22:37:13 +0700

    save

---
+++ problems/295-find-median-from-data-stream/medianfinder.go
@@ -0,0 +1,15 @@
+package findmedianfromdatastream
+
+type MedianFinder struct {
+}
+
+func Constructor() MedianFinder {
+	return MedianFinder{}
+}
+
+func (m *MedianFinder) AddNum(num int) {
+}
+
+func (m *MedianFinder) FindMedian() float64 {
+	return 0.0
+}
---
+++ problems/295-find-median-from-data-stream/medianfinder_test.go
@@ -0,0 +1,40 @@
+package findmedianfromdatastream
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMedianFinder(t *testing.T) {
+	obj := Constructor()
+	obj.AddNum(1)
+	obj.AddNum(2)
+	assert.Equal(t, 1.5, obj.FindMedian())
+	obj.AddNum(3)
+	assert.Equal(t, 2.0, obj.FindMedian())
+
+	obj = Constructor()
+	obj.AddNum(6)
+	assert.Equal(t, 6.0, obj.FindMedian())
+	obj.AddNum(10)
+	assert.Equal(t, 8.0, obj.FindMedian())
+	obj.AddNum(2)
+	assert.Equal(t, 6.0, obj.FindMedian())
+	obj.AddNum(6)
+	assert.Equal(t, 6.0, obj.FindMedian())
+	obj.AddNum(5)
+	assert.Equal(t, 6.0, obj.FindMedian())
+	obj.AddNum(0)
+	assert.Equal(t, 5.5, obj.FindMedian())
+	obj.AddNum(6)
+	assert.Equal(t, 6.0, obj.FindMedian())
+	obj.AddNum(3)
+	assert.Equal(t, 5.5, obj.FindMedian())
+	obj.AddNum(1)
+	assert.Equal(t, 5.0, obj.FindMedian())
+	obj.AddNum(0)
+	assert.Equal(t, 4.0, obj.FindMedian())
+	obj.AddNum(0)
+	assert.Equal(t, 3.0, obj.FindMedian())
+}
