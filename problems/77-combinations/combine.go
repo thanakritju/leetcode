2fc9e676413168f42cdf30f4982d66a2e1b46dfe
thanakritju <thanakrit.ju.work@gmail.com>
2022-04-01 15:29:42 +0700

    finish

--- problems/77-combinations/combine.go
+++ problems/77-combinations/combine.go
@@ -1,5 +1,27 @@
 package combinations
 
 func combine(n int, k int) [][]int {
-	return [][]int{}
+	ans := [][]int{}
+	for i := 1; i <= n; i++ {
+		ans = combineRecur([]int{i}, ans, n, k)
+	}
+	return ans
+}
+
+func combineRecur(arr []int, answer [][]int, n, k int) [][]int {
+	if len(arr) == k {
+		answer = append(answer, arr)
+	} else {
+		for i := arr[len(arr)-1] + 1; i <= n; i++ {
+			newArr := getCopy(arr)
+			answer = combineRecur(append(newArr, i), answer, n, k)
+		}
+	}
+	return answer
+}
+
+func getCopy(src []int) []int {
+	dst := make([]int, len(src))
+	copy(dst, src)
+	return dst
 }
