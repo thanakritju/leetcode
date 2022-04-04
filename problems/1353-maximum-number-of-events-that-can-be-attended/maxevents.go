cec64683020d105d1770eca8d97435c5aa2b0f33
thanakritju <thanakrit.ju.work@gmail.com>
2022-04-04 23:02:21 +0700

    Save

--- problems/1353-maximum-number-of-events-that-can-be-attended/maxevents.go
+++ problems/1353-maximum-number-of-events-that-can-be-attended/maxevents.go
@@ -1,5 +1,66 @@
 package maxevents
 
+import (
+	"container/heap"
+	"sort"
+)
+
+type event [][]int
+type minHeap [][]int
+
 func maxEvents(events [][]int) int {
-	return 0
+	allevents := event(events)
+	attendables := minHeap([][]int{})
+	heap.Init(&attendables)
+	heap.Init(&allevents)
+	joined := 0
+	for day := 1; day <= 10000; day++ {
+		for len(allevents) > 0 && allevents[0][0] <= day {
+			heap.Push(&attendables, heap.Pop(&allevents))
+		}
+
+		for len(attendables) > 0 && attendables[0][1] < day {
+			heap.Pop(&attendables)
+		}
+
+		if len(attendables) > 0 && attendables[0][1] <= day && attendables[0][0] >= day {
+			joined++
+		}
+	}
+	return joined
+}
+
+func (e event) Len() int { return len(e) }
+func (e event) Less(i, j int) bool {
+	return e[i][0] < e[j][0]
+}
+func (e event) Swap(i, j int) { e[i], e[j] = e[j], e[i] }
+func (e *event) Push(x interface{}) {
+	*e = append(*e, x.([]int))
+}
+func (e *event) Pop() interface{} {
+	old := *e
+	n := len(old)
+	x := old[n-1]
+	*e = old[0 : n-1]
+	return x
+}
+
+func (h minHeap) Len() int           { return len(h) }
+func (h minHeap) Less(i, j int) bool { return h[i][1] < h[j][1] }
+func (h minHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *minHeap) Push(x interface{}) {
+	*h = append(*h, x.([]int))
+}
+func (h *minHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func sortEvents(events [][]int) {
+	e := event(events)
+	sort.Sort(&e)
 }
--- problems/1353-maximum-number-of-events-that-can-be-attended/maxevents_test.go
+++ problems/1353-maximum-number-of-events-that-can-be-attended/maxevents_test.go
@@ -13,10 +13,29 @@ func TestMaxEvents(t *testing.T) {
 	}{
 		{[][]int{{1, 2}, {2, 3}, {3, 4}}, 3},
 		{[][]int{{1, 2}, {2, 3}, {3, 4}, {1, 2}}, 4},
+		{[][]int{{1, 4}, {4, 4}, {2, 2}, {3, 4}, {1, 1}}, 4},
+		{[][]int{{1, 2}, {2, 2}, {3, 3}, {3, 4}, {3, 4}}, 4},
+		{[][]int{{1, 2}, {1, 2}, {3, 3}, {1, 5}, {1, 5}}, 5},
+		{[][]int{{1, 5}, {1, 5}, {1, 5}, {2, 3}, {2, 3}}, 5},
 	}
 
 	for _, test := range tests {
 		actual := maxEvents(test.events)
 		assert.Equal(t, test.expected, actual, test)
+	}
+}
+
+func TestSortEvents(t *testing.T) {
+	var tests = []struct {
+		events   [][]int
+		expected [][]int
+	}{
+		{[][]int{{1, 2}, {2, 3}, {3, 4}}, [][]int{{1, 2}, {2, 3}, {3, 4}}},
+		{[][]int{{3, 4}, {1, 3}, {2, 3}, {1, 2}}, [][]int{{1, 2}, {1, 3}, {2, 3}, {3, 4}}},
+	}
+
+	for _, test := range tests {
+		sortEvents(test.events)
+		assert.Equal(t, test.expected, test.events, test)
 	}
 }
