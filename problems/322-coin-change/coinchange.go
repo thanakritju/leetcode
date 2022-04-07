8045a2a4a748410ed65cda5d013725d10f913dea
thanakritju <thanakrit.ju.work@gmail.com>
2022-04-07 22:33:44 +0700

    learn dp

--- problems/322-coin-change/coinchange.go
+++ problems/322-coin-change/coinchange.go
@@ -1,5 +1,35 @@
 package coinchange
 
+import (
+	"math"
+	"sort"
+)
+
 func coinChange(coins []int, amount int) int {
-	return 0
+	dp := make([]int, amount+1)
+	sort.Ints(coins)
+	for i := 1; i <= amount; i++ {
+		dp[i] = math.MaxInt32
+		for _, c := range coins {
+			if i-c < 0 {
+				break
+			}
+
+			if dp[i-c] != math.MaxInt32 {
+				dp[i] = min(dp[i], 1+dp[i-c])
+			}
+		}
+	}
+
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
