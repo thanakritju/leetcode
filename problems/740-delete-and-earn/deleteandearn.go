c4569b2bb3529c8edda2868b7b74d47a754c52de
thanakritju <thanakrit.ju.work@gmail.com>
2022-05-17 13:49:54 +0700

    finish

--- problems/740-delete-and-earn/deleteandearn.go
+++ problems/740-delete-and-earn/deleteandearn.go
@@ -1,5 +1,40 @@
 package deleteandearn
 
 func deleteAndEarn(nums []int) int {
-	return 0
+	var hash = make(map[int]int)
+	var memo [100000]int
+	maxValue := -1
+	for _, num := range nums {
+		if num > maxValue {
+			maxValue = num
+		}
+		_, ok := hash[num]
+		if ok {
+			hash[num] += 1
+		} else {
+			hash[num] = 1
+		}
+	}
+	for i := 1; i <= maxValue; i++ {
+		dp(&memo, hash, i)
+	}
+
+	return memo[maxValue]
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func dp(memo *[100000]int, hash map[int]int, n int) int {
+	if n == 1 {
+		memo[n] = n * hash[n]
+	} else {
+		memo[n] = max(n*hash[n]+memo[n-2], memo[n-1])
+	}
+
+	return memo[n]
 }
--- problems/740-delete-and-earn/deleteandearn_test.go
+++ problems/740-delete-and-earn/deleteandearn_test.go
@@ -13,6 +13,8 @@ func TestDeleteAndEarn(t *testing.T) {
 	}{
 		{[]int{2, 2, 3, 3, 3, 4}, 9},
 		{[]int{3, 4, 2}, 6},
+		{[]int{1, 1, 1, 2, 2, 3, 4, 4}, 12},
+		{[]int{4, 10, 10, 8, 1, 4, 10, 9, 7, 6}, 53},
 	}
 
 	for _, test := range tests {
