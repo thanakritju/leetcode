cace2b10752bc2e90e612fb9813c853edbb27585
thanakritju <thanakrit.ju.work@gmail.com>
2022-07-23 14:36:28 +0700

    save

--- problems/1235-maximum-profit-in-job-scheduling/jobscheduling.go
+++ problems/1235-maximum-profit-in-job-scheduling/jobscheduling.go
@@ -1,5 +1,25 @@
 package maximumprofitinjobscheduling
 
+import "sort"
+
+type Job struct {
+	startTime int
+	endTime   int
+	profit    int
+}
+type Jobs []Job
+
+func (j Jobs) Len() int { return len(j) }
+func (j Jobs) Less(x, y int) bool {
+	return j[x].startTime < j[y].startTime
+}
+func (j Jobs) Swap(x, y int) { j[x], j[y] = j[y], j[x] }
+
 func jobScheduling(startTime []int, endTime []int, profit []int) int {
-	return 0
+	var jobs Jobs
+	for i := 0; i < len(startTime); i++ {
+		jobs = append(jobs, Job{startTime: startTime[i], endTime: endTime[i], profit: profit[i]})
+	}
+	sort.Sort(jobs)
+	return len(jobs)
 }
